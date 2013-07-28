a6ad8a8dcbf0b8981f7a841e2b4d39197ab8742d
David Stephens <davidstephens92@gmail.com>
2013-07-28 16:57:47 -0700

    Moved to Visual Studio.

--- app.ts
+++
@@ -1,9 +0,0 @@
-//Testing to see if comments are emitted.
-class Greeter {
-	greet(name: string){
-		alert("Hey there, " + name + "!");
-	}
-}
-
-var myGreeter = new Greeter();
-myGreeter.greet("David");
