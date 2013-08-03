ce2fee217a33f072b065b227ea9c5c1f7b4b6c74
David Stephens <davidstephens92@gmail.com>
2013-08-03 15:51:26 -0700

    Initial front-end experiments
    Added folders to solution, added a CSS responsive grid library, played
    around with how to layout elements on page.

---
+++ SeeFunction/SeeFunction/Scripts/app.ts
@@ -0,0 +1,27 @@
+//Handler for the window load event
+window.onload = () => {
+
+};
+
+//Handler for the file uploader
+//From http://www.matlus.com/html5-file-upload-with-progress/
+//This site seems better: http://www.html5rocks.com/en/tutorials/file/dndfiles/
+var fileUploader = <HTMLInputElement> document.getElementById("fileUploader");
+fileUploader.onchange = () => {
+    var file = fileUploader.files[0];
+    if (file) {
+        var fileSize = "0";
+        if (file.size > 1024 * 1024 * 1024) {
+            fileSize = (Math.round(file.size * 100 / (1024 * 1024 * 1024)) / 100).toString() + ' GB';
+        } else if (file.size > 1024 * 1024) {
+            fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + ' MB';
+        } else {
+            fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + ' KB';
+        }    
+
+        
+        document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
+        document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize;
+        document.getElementById('fileType').innerHTML = 'Type: ' + file.type;
+    }
+};
--- SeeFunction/SeeFunction/fileUploader.ts
+++
@@ -1,9 +0,0 @@
-class FileUploader {
-    private fileUploadElement: HTMLInputElement;
-
-    constructor(private fileUploadElementId: string) {
-        this.fileUploadElement = <HTMLInputElement> document.getElementById(fileUploadElementId);
-    }
-
-    
-}
