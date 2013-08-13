27458aa051f2973a6a33ee36ca1381189e6e8a3b
David Stephens <davidstephens92@gmail.com>
2013-08-13 11:51:56 -0500

    Working towards basic UI
    Added jQuery. File uploading now displays the text of the file.

--- SeeFunction/SeeFunction/Scripts/app.ts
+++ SeeFunction/SeeFunction/Scripts/app.ts
@@ -17,11 +17,21 @@ fileUploader.onchange = () => {
             fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + ' MB';
         } else {
             fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + ' KB';
-        }    
+        }
 
-        
         document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
         document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize;
         document.getElementById('fileType').innerHTML = 'Type: ' + file.type;
+        
+        var fileReader = new FileReader();
+        fileReader.readAsText(file);
+        fileReader.onload = (loadEvent: ProgressEvent) => {
+            var fileContents = (<FileReader>loadEvent.target).result;
+            $("#fileContents").text(fileContents);
+        };
+
+        fileReader.onerror = (errorEvent) => {
+            $("#fileContents").text("An error occurred while reading the file.");
+        };
     }
 };
